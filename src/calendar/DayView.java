4bee3dabec753edcaa2343423563da372d78e06e
Alein Bartolome <aleinbennett.bartolome@sjsu.edu>
2020-07-29 18:46:36 -0700

    Started writing JavaDoc comments and tweaking WeekView JComponents

--- src/calendar/DayView.java
+++ src/calendar/DayView.java
@@ -1,70 +1,75 @@
 package calendar;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.time.LocalDate;
 import java.util.TreeSet;
 
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.event.ChangeEvent;
-
+/**
+ * 
+ * */
 public class DayView extends JPanel implements CalendarView{
 	
 	private CalendarModel model;
 	private LocalDate now;
 	private TreeSet<Event> events;
-
 	private LocalDate startDate;
-	private LocalDate endDate;
 	
+	/**
+	 * DayView constructor
+	 * */
 	public DayView(CalendarModel m) {
 		this.model = m;
-		this.setLayout(new BorderLayout());
 		events = m.getEvents();
 		display(now);
 	}
 
+	/**
+	 * Creates the JComponents for the GUI of the calendar and calls displayEvents() to add the events for the
+	 * specific day
+	 * */
 	public void display(LocalDate date) {
 		// Clear old components
 		this.removeAll();
 		this.revalidate();
 		
 		now = model.getSelectedDate();
-		
+		this.setLayout(new BorderLayout());
 		// variables to get the month, day, year, and day of the week
 		String currentMonth = now.getMonth().name();
 		String currentDay = now.getDayOfWeek().name();
 		int day = now.getDayOfMonth();
 		int year = now.getYear();
 		
-		// creates the left and right buttons
-		JButton previousButton = new JButton("<");
-		JButton nextButton = new JButton(">");
-		
 		// creates the title
-		JLabel dayTitle = new JLabel(" " + currentMonth + " " + day + ", " + year);
+		JLabel dayTitle = new JLabel(currentMonth + " " + day + ", " + year);
 
 		
 		// day of the week label
 		JLabel dayName = new JLabel(currentDay.substring(0,3), SwingConstants.CENTER);
 		
 		JPanel dayPanel = new JPanel();	
-		dayPanel.setLayout(new GridBagLayout());
+		//dayPanel.setPreferredSize(new Dimension(100,50));
+		dayPanel.setLayout(new GridLayout(3,1));
 		GridBagConstraints c = new GridBagConstraints();
-
+		c.fill = GridBagConstraints.HORIZONTAL;
 		// current day label
 		JLabel dayNumber = new JLabel(Integer.toString(day), SwingConstants.CENTER);	
 
 		// add all the components into the panel together
 		c.gridx = 0;
 		c.gridy = 0;
-
 		dayPanel.add(dayTitle, c);
 		
 		c.gridx = 0;
@@ -74,26 +79,33 @@ public class DayView extends JPanel implements CalendarView{
 		c.gridx = 0;
 		c.gridy = 2;
 		dayPanel.add(dayNumber, c);
-
-
+		
+		c.gridx = 0;
+		c.gridy = 0;
 		this.add(dayPanel, BorderLayout.NORTH);
 		
 		// insert method to call for "events"
-		displayEvents();
+		c.gridx = 0;
+		c.gridy = 1;
+		this.add(displayEvents(), BorderLayout.WEST);
 	}
 	
-	public void displayEvents() {
-		
+	/**
+	 * Method stores all the events into a TreeSet and places the events into a label and returns a panel.
+	 * */
+	public JPanel displayEvents() {
+		startDate = LocalDate.of(now.getYear(), now.getMonthValue(), now.getDayOfMonth());
 		JTextArea eventsLabel = new JTextArea();
-		
-		JScrollPane scroll = new JScrollPane(eventsLabel);
-		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		
 		TreeSet<LocalDate> dates = new TreeSet<LocalDate>();
-		//LocalDate current = startDate;
+		LocalDate current = startDate;
+		while(current.equals(startDate)) {
+			dates.add(current);
+			current = current.plusDays(1);
+		}
 		
 		String eventDisplay = "";
-		
+		JPanel newPanel = new JPanel();
+
 		for(LocalDate day : dates) {
 			boolean noEvents = true;
 			for(Event e : events) {
@@ -105,17 +117,12 @@ public class DayView extends JPanel implements CalendarView{
 					eventDisplay = eventDisplay + "  " + e.toStringForDayView() + "\n";
 				}
 			}
-		}
-		
+		}// end of for loop
 		eventsLabel.setText(eventDisplay);
-		
-		this.add(scroll);
-		
-		this.revalidate();
-		this.repaint();
+		newPanel.add(eventsLabel);
+		return newPanel;
 	}
 	
-	@Override
 	public void stateChanged(ChangeEvent arg0) {
 		this.display(now);
 	}
--- src/calendar/WeekView.java
+++ src/calendar/WeekView.java
@@ -2,14 +2,15 @@ package calendar;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.time.LocalDate;
 import java.util.TreeSet;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import javax.swing.JTextArea;
 import javax.swing.event.ChangeEvent;
 
 public class WeekView extends JPanel implements CalendarView{
@@ -18,10 +19,11 @@ public class WeekView extends JPanel implements CalendarView{
 	private CalendarModel model;
 	private TreeSet<Event> events;
 	private LocalDate now;
-	
+	private LocalDate startDate;
+	private LocalDate endDate;
 	public WeekView(CalendarModel m) {
-		this.setLayout(new BorderLayout());
 		this.model = m;
+		events = m.getEvents();
 		display(now);
 		
 	}
@@ -30,9 +32,8 @@ public class WeekView extends JPanel implements CalendarView{
 		// Clear old components
 		this.removeAll();
 		this.revalidate();
-		
 		now = model.getSelectedDate();
-		
+		this.setLayout(new BorderLayout());
 		// variables to get the month, day, year, and day of the week
 		String currentMonth = now.getMonth().name();
 		int day = now.getDayOfMonth();
@@ -41,44 +42,41 @@ public class WeekView extends JPanel implements CalendarView{
 		// creates the title
 		JLabel dayTitle = new JLabel();
 		if(monthCheck(day) == 1) {
-			dayTitle = new JLabel(" " + currentMonth.substring(0,3) + " - " + now.plusMonths(1).getMonth().name().substring(0,3) + " " + year);
+			dayTitle = new JLabel(currentMonth.substring(0,3) + " - " + now.plusMonths(1).getMonth().name().substring(0,3) + " " + year);
 		}
 		
 		else if(monthCheck(day) == -1) {
-			dayTitle = new JLabel(" " + now.minusMonths(1).getMonth().name().substring(0,3) + " - " + currentMonth.substring(0,3) + " " + year);
+			dayTitle = new JLabel(now.minusMonths(1).getMonth().name().substring(0,3) + " - " + currentMonth.substring(0,3) + " " + year);
 		}
 
 		else {
-			dayTitle = new JLabel(" " + currentMonth + " " + year);
+			dayTitle = new JLabel(currentMonth + " " + year);
 		}
-		// holds the previous and next buttons and date
-		JPanel firstPanel = new JPanel();
-		firstPanel.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
 
-		
+		JPanel firstPanel = new JPanel();
+		firstPanel.setLayout(new GridLayout(3,1));
+		//firstPanel.setPreferredSize(new Dimension(100,100));
+		GridBagConstraints c = new GridBagConstraints();
 		c.gridx = 0;
 		c.gridy = 0;
-		c.fill = GridBagConstraints.HORIZONTAL;
-		firstPanel.add(dayTitle, c);
+		firstPanel.add(dayTitle,c);
 
-		
 		// second panel holds the days of the week
 		JPanel secondPanel = new JPanel();
-		
-		String[] days = {"SAT", "FRI", "THU", "WED", "TUE", "MON", "SUN"};
+		secondPanel.setLayout(new GridBagLayout());
+		String[] days = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
 		for(int i = 0; i < days.length; i++) {
-			c.fill = GridBagConstraints.HORIZONTAL;
-			secondPanel.add(new JLabel(days[i]), c, SwingConstants.CENTER);
-			// for some reason, this is adding the labels in reverse order of the String[] days
+			c.gridx = i;
+			c.gridy = 1;
+			c.ipadx = 20;
+			c.fill = GridBagConstraints.CENTER;
+			secondPanel.add(new JLabel(days[i]), c);
 		}
-		c.gridx = 0;
-		c.gridy = 1;
-		c.gridwidth = 3;
-		firstPanel.add(secondPanel,c);
+		
 		this.add(firstPanel, BorderLayout.NORTH);
-		displayDayView();
-
+		displayDayView(secondPanel, c);
+		revalidate();
+		repaint();
 	}
 	
 	
@@ -96,76 +94,114 @@ public class WeekView extends JPanel implements CalendarView{
 	/**
 	 * Prints out the day buttons
 	 * */
-	public void displayDayView() {
+	public void displayDayView(JPanel secondPanel, GridBagConstraints c) {
 		LocalDate today = model.getSelectedDate();
-		
-		JPanel daysPanel = new JPanel();
-		daysPanel.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
 		
 		switch(today.getDayOfWeek().ordinal()) {
 		
 		// if the current day starts on Monday
 		case 0:
 			LocalDate oneBeforeToday = today.minusDays(1);
-			addDayNumber(oneBeforeToday, today, daysPanel);
+			this.addDayNumber(oneBeforeToday, today, secondPanel,c);
+			displayEvents(oneBeforeToday,c);
 			break;
 		
 		// if the current day starts on Tuesday	
 		case 1:
 			LocalDate twoBeforeToday = today.minusDays(2);
-			addDayNumber(twoBeforeToday, today, daysPanel);
+			addDayNumber(twoBeforeToday, today, secondPanel,c);
+			displayEvents(twoBeforeToday,c);
 			break;
 		
 		// if the current day starts on Wednesday
 		case 2:
-			LocalDate threeBeforeToday = today.minusDays(2);
-			addDayNumber(threeBeforeToday, today, daysPanel);
+			System.out.println("cake");
+			LocalDate threeBeforeToday = today.minusDays(3);
+			addDayNumber(threeBeforeToday, today, secondPanel,c);
+			displayEvents(threeBeforeToday,c);
 			break;
 		
 		// if the current day starts on Thursday
 		case 3:
 			LocalDate fourBeforeToday = today.minusDays(4);
-			addDayNumber(fourBeforeToday, today, daysPanel);
+			addDayNumber(fourBeforeToday, today, secondPanel,c);
+			displayEvents(fourBeforeToday,c);
 			break;
 		
 		//if the current day starts on Friday
 		case 4:
 			LocalDate fiveBeforeToday = today.minusDays(5);
-			addDayNumber(fiveBeforeToday, today, daysPanel);
+			addDayNumber(fiveBeforeToday, today, secondPanel,c);
+			displayEvents(fiveBeforeToday,c);
 			break;
 		
 		//if the current day starts on Saturday
 		case 5:
 			LocalDate sixBeforeToday = today.minusDays(6);
-			addDayNumber(sixBeforeToday, today, daysPanel);
+			addDayNumber(sixBeforeToday, today, secondPanel,c);
+			displayEvents(sixBeforeToday,c);
 			break;
 			
 		// if the current day starts on Sunday
 		case 6:
 			LocalDate sunday = today;
-			addDayNumber(sunday, today, daysPanel);
+			addDayNumber(sunday, today, secondPanel,c);
+			displayEvents(today,c);
 			break;
 		}// end of switch case
 	}// end of displayDayView
 	
 	// method to fill in the days of the week
-	public void addDayNumber(LocalDate day, LocalDate today, JPanel daysPanel) {
+	public void addDayNumber(LocalDate day, LocalDate today, JPanel secondPanel, GridBagConstraints c) {
 		for (int i = 0; i < 7; i++) {
 			if(day.getDayOfMonth() == today.getDayOfMonth()) {
-				JButton todayButton = new JButton(Integer.toString(day.getDayOfMonth()));
-				todayButton.setForeground(Color.WHITE);
-				todayButton.setBackground(Color.BLUE);
-				daysPanel.add(todayButton);
+				c.gridx = i;
+				c.gridy = 2;
+				JLabel todayLabel = new JLabel(Integer.toString(day.getDayOfMonth()));
+				todayLabel.setForeground(Color.RED);
+				todayLabel.setBackground(Color.BLUE);
+				secondPanel.add(todayLabel,c);
 				
 				day = day.plusDays(1);
 			}
 			else {
-				daysPanel.add(new JButton(Integer.toString(day.getDayOfMonth())));
+				c.gridx = i;
+				c.gridy = 2;
+				secondPanel.add(new JLabel(Integer.toString(day.getDayOfMonth())),c);
 				day = day.plusDays(1);
 			}
 		}
-		this.add(daysPanel, BorderLayout.CENTER);
+		this.add(secondPanel, BorderLayout.WEST);
+	}
+	
+	public void displayEvents(LocalDate sunday, GridBagConstraints c) {
+		startDate = LocalDate.of(sunday.getYear(), sunday.getMonthValue(), sunday.getDayOfMonth());
+		endDate = startDate.plusDays(6);
+		JTextArea eventsLabel = new JTextArea();
+		TreeSet<LocalDate> dates = new TreeSet<LocalDate>();
+		LocalDate current = startDate;
+		while(!current.equals(endDate)) {
+			dates.add(current);
+			current = current.plusDays(1);
+		}
+	
+		String eventDisplay = "";
+		JPanel newPanel = new JPanel();
+
+		for(LocalDate day : dates) {
+			boolean noEvents = true;
+			for(Event e : events) {
+				if(e.occursOn(day)) {
+					if(noEvents) {
+						eventDisplay = eventDisplay + "\n" + day + "\n";
+					}
+					noEvents = false;
+					eventDisplay = eventDisplay + "  " + e.toStringForDayView() + "\n";
+				}
+			}
+		}// end of for loop
+		eventsLabel.setText(eventDisplay);
+		this.add(eventsLabel);
 	}
 	
 	public void stateChanged(ChangeEvent arg0) {
